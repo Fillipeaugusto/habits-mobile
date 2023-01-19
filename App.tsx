d2a085c94802052d1cff922ed9aa07210549be7f
Fillipeaugusto <fillipeaugustoribeiro@gmail.com>
2023-01-19 09:12:54 -0300

    finish screens

--- App.tsx
+++ App.tsx
@@ -1,49 +1,35 @@
 import './src/lib/dayjs';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import {
-	useFonts,
-	Inter_400Regular,
-	Inter_600SemiBold,
-	Inter_700Bold,
-	Inter_800ExtraBold,
+
+import { StatusBar } from 'react-native';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
-	const [fontsLoaded] = useFonts({
-		Inter_400Regular,
-		Inter_600SemiBold,
-		Inter_700Bold,
-		Inter_800ExtraBold,
-	});
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
 
-	if (!fontsLoaded) {
-		return <Loading />;
-	}
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
 
-	return (
-		<View style={styles.container}>
-			<Home />
-			<StatusBar
-				barStyle="light-content"
-				backgroundColor="transparent"
-				translucent
-			/>
-		</View>
-	);
+  return (
+    <>
+      <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
+  );
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#09090A',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	text: {
-		color: '#FFF',
-		fontFamily: 'Inter_800ExtraBold',
-	},
-});
